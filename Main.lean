9f7ae410cfaa9f44a0528631ae2b0ef8fbce2025
FunkyCosmonaut <FunkyCosmonaut@protonmail.com>
2025-12-21 08:16:22 -0900

    test 2

--- Main.lean
+++ Main.lean
@@ -1,9 +1,19 @@
 import FirstSipOfLean
 open Classical
 
+def main : IO Unit := do
+  IO.println "Test"
+
 theorem and_commutative (p q : Prop) : p ∧ q → q ∧ p :=
   fun hpq : p ∧ q =>
   have hp : p := And.left hpq
   have hq : q := And.right hpq
   show q ∧ p from And.intro hq hp
 
+--Declaring objects and checking types
+def m : Nat := 1
+#check m
+def b1 : Bool := true
+#check b1
+def b2 : Bool := false
+#check b2
