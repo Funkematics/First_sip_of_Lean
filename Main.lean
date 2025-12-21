09bd6857345dbab1e40452b5ff1d430eb918d11a
FunkyCosmonaut <FunkyCosmonaut@protonmail.com>
2025-12-21 08:10:38 -0900

    init and first test

---
+++ Main.lean
@@ -0,0 +1,9 @@
+import FirstSipOfLean
+open Classical
+
+theorem and_commutative (p q : Prop) : p ∧ q → q ∧ p :=
+  fun hpq : p ∧ q =>
+  have hp : p := And.left hpq
+  have hq : q := And.right hpq
+  show q ∧ p from And.intro hq hp
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# First_sip_of_Lean
